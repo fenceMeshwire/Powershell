c8a84fec31d2eb3891024d75c1ee636f3c785914
fenceMeshwire <94779778+fenceMeshwire@users.noreply.github.com>
2022-09-06 15:48:35 +0200

    Create 01_data_types.ps1

---
+++ 01_data_types.ps1
@@ -0,0 +1,16 @@
+# Data Types
+
+# Assign variables
+$integer = 11 # Integer
+$text_as_number = 'eleven' # string
+$is_available = $false # boolean
+
+# Return values of variables with Name / Value table
+Get-Variable -Name integer
+Get-Variable -Name text_as_number
+Get-Variable -Name is_available
+
+# Simply return the variable values with the next line
+$integer
+$text_as_number
+$is_available
