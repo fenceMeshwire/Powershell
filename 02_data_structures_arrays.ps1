cd465315bd28f8b2813749608534e8e812915bfe
fenceMeshwire <94779778+fenceMeshwire@users.noreply.github.com>
2022-09-08 11:41:46 +0200

    Create 02_data_structures_arrays.ps1

---
+++ 02_data_structures_arrays.ps1
@@ -0,0 +1,20 @@
+# Create Array
+$nameArr = @('Ann','James','Matthew','Sally','Albert','Bert')
+$nameArr
+
+# Read Array Elements
+$nameArr[0]    # Get first element
+$nameArr[-1]   # Get last element
+$nameArr[1]    # Get n element
+
+# Modify Array Elements
+$nameArr[1] = 'John'
+$nameArr[1]
+
+# Add Array Elements
+$nameArr = $nameArr + 'Sofia'
+$nameArr
+$nameArr += 'Steve'
+$nameArr
+$nameArr += @('Eric', 'David')
+$nameArr
