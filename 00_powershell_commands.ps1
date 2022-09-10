b6197e7a3bb92ba0ffdca83e3f33717152a4b788
fenceMeshwire <94779778+fenceMeshwire@users.noreply.github.com>
2022-09-10 13:15:36 +0200

    Update 00_powershell_commands.ps1

--- 00_powershell_commands.ps1
+++ 00_powershell_commands.ps1
@@ -7,4 +7,5 @@ Get-Variable -Name *Preference
 $ErrorActionPreference = 'SilentlyContinue'
 
 # Show last exit code (1: exit code for error, 0: exit code for no error)
+# The corresponding Linux command: # echo $? 
 $LASTEXITCODE
