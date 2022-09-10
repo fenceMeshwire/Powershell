51e1e64bbb0f7df2a922f669acb2acb1dd53a991
fenceMeshwire <94779778+fenceMeshwire@users.noreply.github.com>
2022-09-10 18:07:03 +0200

    Update 00_powershell_commands.ps1

--- 00_powershell_commands.ps1
+++ 00_powershell_commands.ps1
@@ -15,3 +15,10 @@ $ErrorActionPreference = 'SilentlyContinue'
 # Show last exit code (1: exit code for error, 0: exit code for no error)
 # The corresponding Linux command: # echo $? 
 $LASTEXITCODE
+
+# Start update service 'wuauserv'
+# Combining service and hostname to be executed on the local computer:
+# Corresponding Linux command: $ sudo apt update
+
+$service = [PSCustomObject]@{Name = 'wuauserv'; ComputerName = hostname}
+$service | Get-Service
