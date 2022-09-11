30f6d57c11f80b83eecf6466651c27c948d01803
fenceMeshwire <94779778+fenceMeshwire@users.noreply.github.com>
2022-09-11 15:29:32 +0200

    Create 05_If_Statement.ps1

---
+++ 05_If_Statement.ps1
@@ -0,0 +1,24 @@
+# The If.Statement in PowerShell
+
+# Example:
+
+$ValueA = 2
+$ValueB = 4
+
+if ($ValueA -gt $ValueB) {
+    "Value A is greater than value B"
+} elseif ($ValueA -lt $ValueB) {
+    "Value A is less than value B"
+} else {
+    "Value A is equal to value B."
+}
+
+# Fundamental structure:
+if (condition) {
+  # Code block executed if condition is True.
+} else {
+  # Code block executed if condition is False.
+}
+
+# Values for conditions can be compared with: 
+# -eq, -ne, -lt, -gt, -ge, -le, -contains
