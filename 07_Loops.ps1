f277a427d23d9da6c659acbcaedf30fcfa827b49
fenceMeshwire <94779778+fenceMeshwire@users.noreply.github.com>
2022-09-12 09:51:13 +0200

    Create 07_Loops.ps1

---
+++ 07_Loops.ps1
@@ -0,0 +1,30 @@
+# ForEach-Loop
+# =========================================
+$letters = @('a', 'b', 'c', 'd', 'e', 'f')
+
+foreach ($letter in $letters) {
+    "Printing current letter: $letter"
+}
+
+# For-Loop
+# =========================================
+$romanliterals = @{
+1='I'; 2='II'; 3='III'; 4='IV'; 5='V'; 6='VI'; 7='VII'; 8='VIII';9='IX'
+}
+
+$lengthLiterals = $romanliterals.count
+$lengthLiterals += 1
+
+for ($i = 1; $i -lt $lengthLiterals; $i++) {
+    "$i corresponds to: " +  $romanliterals[$i]
+}
+
+# While-Loop
+# =========================================
+$valueA = 0
+$valueB = 5
+
+while($valueA -lt $valueB) {
+    $valueA += 1
+    "$valueA <= $valueB"
+}
