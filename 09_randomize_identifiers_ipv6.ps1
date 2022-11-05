c5921e2d137e4f3c92f0d799816df61d979248a5
fenceMeshwire <94779778+fenceMeshwire@users.noreply.github.com>
2022-11-05 13:25:37 +0100

    Create 09_randomize_identifiers_ipv6.ps1
    Purpose: Disable and enable the option RandomizeIdentifiers within the IPv6 protocol.

---
+++ 09_randomize_identifiers_ipv6.ps1
@@ -0,0 +1,17 @@
+# Purpose: Disable and enable the option RandomizeIdentifiers within the IPv6 protocol.
+
+# Show local IPv6 addresses:
+Get-NetIPConfiguration | Select-Object IPv6LinkLocalAddress
+
+# Show IPv6 protocols:
+Get-NetIPv6Protocol
+
+# Disable RandomizeIdentifiers:
+Set-NetIPv6Protocol -RandomizeIdentifiers Disabled
+# Check Status of RandomizeIdentifiers:
+Get-NetIPv6Protocol
+
+# Enable RandomizeIdentifiers:
+Set-NetIPv6Protocol -RandomizeIdentifiers Enabled
+# Check Status of RandomizeIdentifiers:
+Get-NetIPv6Protocol
